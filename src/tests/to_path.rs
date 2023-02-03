d152a44a1722f2b0115449f8abb643eda9607d76
Faouzi <87236@glr.nl>
2023-02-03 21:18:19 +0100

    test route with params

--- src/tests/to_path.rs
+++ src/tests/to_path.rs
@@ -15,4 +15,26 @@ mod tests {
         let route:RouteInfo = RouteParse::to_route(some_path.to_string());
         assert_eq!(route.type_route.unwrap(), "GET");
     }
+
+
+    #[test] 
+    fn test_params(){
+        let some_path = "GET /home/some/home HTTP/1.1";
+        let route:RouteInfo = RouteParse::to_route(some_path.to_string());
+        assert_eq!(route.params.unwrap(), vec!["","home","some", "home"]);
+    }
+
+    #[test]
+    fn test_path(){
+        let some_path = "GET /home/some/home HTTP/1.1";
+        let route:RouteInfo = RouteParse::to_route(some_path.to_string());
+        assert_eq!(route.full_path.unwrap(), "/home/some/home");
+    }
+
+    #[test]
+    fn test_path_with_params(){
+        let some_path = "GET /home/some/home?name=John&age=20 HTTP/1.1";
+        let route:RouteInfo = RouteParse::to_route(some_path.to_string());
+        assert_eq!(route.full_path.unwrap(), "/home/some/home");
+    }
 }
