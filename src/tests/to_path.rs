b83219218c9d2a8eed5a2607eec1ab8cccfe36ac
Faouzi <87236@glr.nl>
2023-02-03 21:14:53 +0100

    fix for tests

--- src/tests/to_path.rs
+++ src/tests/to_path.rs
@@ -1,17 +1,17 @@
 #[cfg(test)]
 mod tests {
-    use crate::router::{RouteParse, RouteInfo};
+    use crate::routef::router::{RouteParse, RouteInfo};
 
     #[test] 
     fn test_parent(){
-        let some_path = "GET home/some/home HTTP/1.1";
+        let some_path = "GET /home/some/home HTTP/1.1";
         let route:RouteInfo= RouteParse::to_route(some_path.to_string());
         assert_eq!(route.parent.unwrap(), "home");
     }
 
     #[test] 
     fn test_type(){
-        let some_path = "GET home/some/home HTTP/1.1";
+        let some_path = "GET /home/some/home HTTP/1.1";
         let route:RouteInfo = RouteParse::to_route(some_path.to_string());
         assert_eq!(route.type_route.unwrap(), "GET");
     }
